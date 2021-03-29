4193fd2d89739a97f263c9de66e7ba2c7760da2a
Alexey <aalekseyf2001@mail.ru>
2021-03-29 20:13:10 +0300

    First commit

--- main.cpp
+++ main.cpp
@@ -1,52 +1,142 @@
 #include <iostream>
+#include <fstream>
+#include <map>
 #include <string>
-#include <math.h>
-
-#define size 12
-#define INVALID 1
-#define FALSE 0
-#define NOROOTS 2
 using namespace std;
 
-void ReadData(double* coefs) {
-	int i = 0;
-	while (i < 3) {
-		cin >> coefs[i];
-		i++;
-	}
-}
-
-void Solve(double* coefs, double &root1, double &root2, int &error) {
-	double a = coefs[0], b = coefs[1], c = coefs[2], D;
-	D = b * b - 4 * a * c;
+void SolveCorrectlyEquation(double a, double b, double c, double& root1, double& root2, int &countOfRoot) {
+	double D = b* b - 4 * a * c;
 	if (D < 0) {
-		error = NOROOTS;
-		return;
+		countOfRoot = 0;
 	}
 	else if (D > 0) {
 		root1 = (-b + sqrt(D)) / (2 * a);
 		root2 = (-b - sqrt(D)) / (2 * a);
+		countOfRoot = 2;
 	}
 	else {
 		root1 = -b / (2 * a);
 		root2 = root1;
+		countOfRoot = 1;
 	}
 }
 
+class Teacher {
+public:
+	string name;
+	map<string, int> results;
+
+	void CheckEquations() {
+		ifstream ansOfStudents("answers.txt");
+		double a, b, c, root1, root2, rootsFromStudent[2];
+		int countOfRoots, countRootsSt;
+		string nameSt, surnameSt;
+
+		while (ansOfStudents >> a >> b >> c >> countRootsSt >> rootsFromStudent[0] >> rootsFromStudent[1] >> nameSt >> surnameSt) {
+			SolveCorrectlyEquation(a, b, c, root1, root2, countOfRoots);
+			if (results.count(nameSt + surnameSt) == 0) {
+				results.emplace(nameSt + " " + surnameSt, 0);
+			}
+			if (root1 == rootsFromStudent[0] && root2 == rootsFromStudent[1] && countOfRoots == countRootsSt) {
+				results.at(nameSt + " " + surnameSt)++;
+			}
+		}
+		ansOfStudents.close();
+	}
+
+	void PrintResults() {
+		cout << "| Name of student - Score \n";
+		for (auto result : results) {
+			cout << "| " << result.first << " - " << result.second << endl;
+		}
+	}
+};
+
+class Student {
+public:
+	int countOfRoot = 0;
+	double root1 = 0, root2 = 0;
+	string firstName;
+	string lastName;
+	Student(string name, string surname) : firstName(name), lastName(surname)
+	{
+	}
+
+	virtual void SolveEquation(double a, double b, double c) {
+		root1 = 0;
+		root2 = 0;
+		countOfRoot = 1;
+	}
+	friend ostream& operator<<(ostream& os, const Student& dt);
+};
+ostream& operator<<(ostream& os, const Student& st)
+{
+	os << st.firstName + " " + st.lastName;
+	return os;
+}
+
+class GoodStudent : public Student {
+public:
+	GoodStudent(string name, string surname) : Student(name, surname) {
+	}
+	void SolveEquation(double a, double b, double c) {
+		SolveCorrectlyEquation(a, b, c, root1, root2, countOfRoot);
+	}
+};
+
+class AverageStudent : public Student {
+public:
+	AverageStudent(string name, string surname) : Student(name, surname) {
+	}
+	void SolveEquation(double a, double b, double c) {
+		if (rand() % 2 == 0) {
+			SolveCorrectlyEquation(a, b, c, root1, root2, countOfRoot);
+		}
+		else {
+			Student::SolveEquation(a, b, c);
+		}
+	}
+};
+
+class BadStudent : public Student {
+public:
+	BadStudent(string name, string surname) : Student(name, surname) {
+	}
+	void SolveEquation(double a, double b, double c) {
+		root1 = 0;
+		countOfRoot = 1;
+	}
+};
+
+void WriteAnswersToFile(ofstream &file, Student st, double a, double b, double c) {
+	file << a << " " << b << " " << c << " ";
+	file << st.countOfRoot << " " << st.root1 << " " << st.root2 << " " << st << "\n";
+}
+
 int main(void) {
-	int error = FALSE;
-	double coefs[3], root1, root2;
+	ifstream equations("equations.txt");
+	ofstream answers("answers.txt");
+	double a, b, c;
+	Student st("A", "A");
+	GoodStudent A("Alex", "Alex");
+	AverageStudent B("Bob", "Bob"), D("Ben", "Ben");
+	BadStudent C("Cat", "Cat");
 
-	cout << "Enter the coefficients a b c in the form \"a b c\"\n";
-	ReadData(coefs);
+	while (equations >> a >> b >> c) {
+		A.SolveEquation(a, b, c);
+		B.SolveEquation(a, b, c);
+		C.SolveEquation(a, b, c);
+		D.SolveEquation(a, b, c);
+		WriteAnswersToFile(answers, A, a, b, c);
+		WriteAnswersToFile(answers, B, a, b, c);
+		WriteAnswersToFile(answers, C, a, b, c);
+		WriteAnswersToFile(answers, D, a, b, c);
+	}
 
-	Solve(coefs, root1, root2, error);
-	if (error == FALSE) {
-		cout << "The fisrt root " << root1 << endl;
-		cout << "The second root "<< root2 << endl;
-	}
-	else if (error == NOROOTS) {
-		cout << "There are no solutions!\n";
-	}
+	answers.close();
+	Teacher t;
+	t.CheckEquations();
+	t.PrintResults();
+	equations.close();
 	return 0;
 }
